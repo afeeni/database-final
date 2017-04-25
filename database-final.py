e94e6713f8d218a8358fc5c4caee06a601ce7c09
Afeeni Phillips <afeeniphillips@Afeenis-MacBook-Pro-3.local>
2017-04-24 20:27:10 -0400

    Own Template

--- database-final.py
+++ database-final.py
@@ -7,8 +7,20 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template ('layout.html')
+def login():
+    return render_template('login.html')
+
+@app.route('/home')
+def index():
+    return render_template('index.html')
+
+@app.route('/viewbill')
+def about():
+    return render_template('viewbill.html')
+
+@app.route('/paybill')
+def contact():
+    return render_template('paybill.html')
 
 
 # Load default config and override config from an environment variable
